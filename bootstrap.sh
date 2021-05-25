376f6c76132faf7e75b19787cf23c38ad9d4aa60
Fitzy <fitzystrikesagain@gmail.com>
2021-05-25 14:30:33 -0500

    Get rid of epoch and parameterize variables

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+# Exports .env variables to local environment
+export "$(grep -E 'PINOT_APP_NAME' docker/.env)"
+export "$(grep -E 'IMPORT_SCRIPT' docker/.env)"
+
 # Imports NOAA storm event data into Apache Pinot
-docker exec -ti pinot_app_noaa bash -c "sh ./import/import-storm-events.sh"
+docker exec -ti "$PINOT_APP_NAME" bash -c "sh $IMPORT_SCRIPT"
 
 # Initializes the Superset application and imports datasources and dashboards
-docker exec -ti -u root superset_app_noaa bash -c "sh ./docker-init.sh"
+docker exec -ti -u root "$SUPERSET_APP_NAME" bash -c "sh ./docker-init.sh"
 
-# Open the browser for Pinot and Superset
-open http://localhost:9000
+# Open the browser for Pinot query console and Superset dashboard page
+open http://localhost:9000/#/query
 open http://localhost:8088
 
 echo "Success! Login to Superset using the credentials admin/admin"
